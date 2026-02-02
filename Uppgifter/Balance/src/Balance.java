694659b020335b3420ee6a7b4dae433fc197504c
Pinkleish <axel.dahl@hotmail.se>
2026-02-02 15:02:32 +0100

    balansera klar

--- Uppgifter/Balance/src/Balance.java
+++ Uppgifter/Balance/src/Balance.java
@@ -1,3 +1,4 @@
+import java.util.EmptyStackException;
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -7,35 +8,65 @@ public class Balance {
         String s = scanner.nextLine();          // Sätt s till strängen med parenteser.
         // Första tecknet är s[0], s[s.Length-1] det sista.
         char[] arr = s.toCharArray();
-        int results = -1;
+        boolean error = false;
 
 
 
 
-        Stack<Character> stack = new Stack<Character>();             // Skapa en stack.
+        Stack<Character> stack = new Stack<Character>();
+        // Skapa en stack.
         // Det är inte säkert att man vill ha just en stack
         // av Character, det beror på hur algorimen
         // implementeras, men en stack behövs.
         // Gör push med stack.push(…), pop med stack.pop()
         // Kolla om stacken är tom med ”stack.isEmpty()”.
-
         // Fyll i själva algoritmen för att kolla s här.
-        if (arr[0] == ')' || arr[0] == ']'){
-            results = 0;
-        }
-        else {
-            stack.push(arr[0]);
-            for (int i = 1; i < s.length(); i++) {
-                if (arr[i] == '(' || arr[i] == '['){
-                    stack.push(arr[i]);
+
+        // Loopa genom listan
+        for (int i = 0; i < s.length();i++){
+            if (arr[i] == '(' || arr[i] == '['){
+                stack.push(arr[i]);
+            }
+
+            // Försöker stänga rund parantes
+            else if(arr[i] == ')'){
+               if (stack.isEmpty()){
+                   error = true;
                 }
-                else{
-                    stack.pop();
+               else {
+                   if (stack.pop() != '('){
+                       error = true;
+                   }
+               }
+            }
+
+            // Försöker stänga hakparantes
+            else if(arr[i] == ']'){
+                if (stack.isEmpty()){
+                    error = true;
+                }
+                else {
+                    if (stack.pop() != '['){
+                        error = true;
+                    }
                 }
             }
-            System.out.println(stack.isEmpty());
 
+            // Annan karaktär har angets
+            else{
+                error = true;
+            }
         }
+
+        // Om inget fel och listan är tom
+        if (!error && stack.isEmpty()){
+            System.out.println(1);
+        }
+        // Ovan stämmer ej
+        else{
+            System.out.println(0);
+        }
+
         // Använd System.out.println(…) för att skriva ut antingen 0 eller 1 som slutresultat.
     }
 }
